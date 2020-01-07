6ab7bf6f7ce657540d1227f0ed29a541eef9d65a
amaralc <amaral.calil@gmail.com>
2020-01-07 16:16:36 -0300

    code: define nova rota post SessionController.store

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 // Imports ---------------------------------------------------------------------
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 // Content ---------------------------------------------------------------------
 const routes = new Router();
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 // Exports ---------------------------------------------------------------------
 export default routes;
