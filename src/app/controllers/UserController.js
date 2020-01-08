580b6159dd47a12b8400ad7c743c718a157057be
amaralc <amaral.calil@gmail.com>
2020-01-08 17:50:28 -0300

    code: cria funcionalidades de atualizacao do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -25,10 +25,40 @@ class UserController {
   // Permite que usuario faca alteracao de seus dados cadastrais
   // Rota nao pode ser acessivel a usuarios que nao estejam logados
   async update(req, res) {
-    console.log(req.userId);
+    // Salva email e antigo password
+    const { email, oldPassword } = req.body;
+
+    // Busca usuario no banco de dados a partir da primary key (pk)
+    const user = await User.findByPk(req.userId);
+
+    // Se usuario estiver mudando de e-mail
+    if (email !== user.email) {
+      // Verifica existencia de usuario antes de cadastrar
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      // Se usuario existir, retorna erro (bad request)
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    // Retorna erro caso antigo password nao combine com o informado E usuario estiver tentando alterar senha
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    // Atualiza usuario
+    const { id, name, provider } = await user.update(req.body);
 
     // Retorna objeto json para cliente
-    return res.json({ ok: true });
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
