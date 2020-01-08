42b8d68b5998aa7be7132f3042dc3cd309208d90
amaralc <amaral.calil@gmail.com>
2020-01-08 17:08:36 -0300

    code: cria rota de alteracao do usuario usando autenticacao

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,16 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 // Content ---------------------------------------------------------------------
 const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// Define middleware global para ser utilizado nas rotas que vierem apos routes.use...
+routes.use(authMiddlewares);
+routes.put('/users', UserController.update);
 
 // Exports ---------------------------------------------------------------------
 export default routes;
